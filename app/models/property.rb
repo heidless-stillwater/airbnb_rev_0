ad976a32575424ac54f799cfe59f53d74708c699
heidless <lockhart.r@gmail.com>
2025-01-05 13:06:18 +0000

    base geo func

--- app/models/property.rb
+++ app/models/property.rb
@@ -6,4 +6,14 @@ class Property < ApplicationRecord
   validates :city, presence: true 
   validates :state, presence: true 
   validates :country, presence: true 
+
+  geocoded_by :address
+#  after_validation :geocode, unless: -> (obj) {obj.latitude.present? and obj.longitude.present? }
+  after_validation :geocode, if: -> { latitude.blank? && longitude.blank? }   # workaround for fake properties
+
+  def address
+    # [address_1, address_2, city, state, country].compact.join(', ')
+    [state, country].compact.join(', ')  # workaround for fake properties
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_04_105231) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_05_120637) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_04_105231) do
     t.datetime "updated_at", null: false
     t.string "address_1"
     t.string "address_2"
+    t.float "latitude"
+    t.float "longitude"
+    t.index ["latitude", "longitude"], name: "index_properties_on_latitude_and_longitude"
   end
 
   create_table "users", force: :cascade do |t|
